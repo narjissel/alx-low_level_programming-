131507f07521647fadb451350b47d960ed76fb75
NARJISS_EL <99508336+narjissel@users.noreply.github.com>
2023-10-12 00:26:47 +0300

    0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s \n", _putchar);
+
+	return (0);
+}
